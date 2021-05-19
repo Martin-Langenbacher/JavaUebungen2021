3abea937845f0116e1f4785df5fd9b8bfe041c73
Martin Langenbacher <martin.langenbacher@telekom.de>
2021-05-19 11:08:06 +0200

    erster Klasse commit

---
+++ KopfbisFussUebungen/src/KopfbisFussBasic/ErsterTestKlasse.java
@@ -0,0 +1,11 @@
+package KopfbisFussBasic;
+
+public class ErsterTestKlasse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Meine erste Test-Klasse");
+
+	}
+
+}
---
+++ KopfbisFussUebungen/src/KopfbisFussBasic/ZweiteTestKlasse.java
@@ -0,0 +1,11 @@
+package KopfbisFussBasic;
+
+public class ZweiteTestKlasse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Meine zweite Klasse... !");
+
+	}
+
+}
